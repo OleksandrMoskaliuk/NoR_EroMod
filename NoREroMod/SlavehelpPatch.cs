be72b30bc83b7b825e996dcf547783f062be6ff7
AlexMercer <redownloader999@gmail.com>
2024-01-02 17:19:59 +0200

    Slave respawn patch.

---
+++ NoREroMod/SlavehelpPatch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+namespace NoREroMod
+{
+    internal class SlavehelpPatch
+    {
+        // Slave always respawn
+        [HarmonyLib.HarmonyPatch(typeof(global::Slavehelp), "help")]
+        [HarmonyLib.HarmonyPostfix]
+        private static void RespawnSlavesAfterHelp(Slavehelp __instance, game_fragmng ___Flagmng, SpawnSlave ___spawnslave)
+        {
+            ___Flagmng._helpslaveStage[___spawnslave._stage, ___spawnslave._SlaveNumber] = false;
+        }
+
+
+
+
+        public SlavehelpPatch()
+        {
+        }
+    }
+
+}
